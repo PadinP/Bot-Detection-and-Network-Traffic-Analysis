56e29057fa43ea676d5a66e122fb258f38e6087b
Alex <alexpp2809@gmail.com>
2025-05-29 19:10:34 -0400

    cambiando imporatciones en el main

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 import uvicorn
-from app.routers import bot_detection,capture_data,orchestrate
+from app.routers import bot_detection_flow
+# from app.routers import bot_detection,capture_data
 
 app = FastAPI()
+
 # app.include_router(capture_data.router)
 # app.include_router(bot_detection.router)
-app.include_router(orchestrate.router)
+app.include_router(bot_detection_flow.router)
 
 if __name__ == "__main__":  
     uvicorn.run("main:app", port=8001, reload=True)
