5374a89439e6c6339d919dbfb730350822c76ec7
Alex <alexpp2809@gmail.com>
2025-06-11 19:02:19 -0400

    corrigiendo como se muestran los logs

--- app/config/settings.py
+++ app/config/settings.py
@@ -6,4 +6,4 @@ LOG_PATH = 'modulo/capturas/logs'
 LOG_FILE = 'bto_logs.log'
 PCAP_FILE = 'modulo/capturas/pcaps/capture.pcap'
 OUTPUT_FOLDER = "modulo/capturas/archivos_filtrados"
-NETWORK_INTERFACE = "br-19b241f0045b"
+NETWORK_INTERFACE = "br-576f9142429a"
